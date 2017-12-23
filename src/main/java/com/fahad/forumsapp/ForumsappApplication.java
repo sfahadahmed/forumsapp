19834d0612abe246959ff780d3e7da730b5c34d3
sfahadahmed <sfahadahmed@gmail.com>
2017-12-23 11:02:56 +0500

    Initial project structure.

---
+++ src/main/java/com/fahad/forumsapp/ForumsappApplication.java
@@ -0,0 +1,12 @@
+package com.fahad.forumsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumsappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fahad/forumsapp/ForumsappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fahad.forumsapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForumsappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
