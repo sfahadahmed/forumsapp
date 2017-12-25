2bcb319365245b078b0ea2719c28dfcce66e1347
sfahadahmed <sfahadahmed@gmail.com>
2017-12-25 20:24:49 +0500

    Adding message source support config.

---
+++ src/main/java/com/fahad/forumsapp/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.fahad.forumsapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+/**
+ * @author Fahad Ahmed
+ */
+@Configuration
+public class AppConfig {
+
+    @Bean
+    @Description("Spring Message Resolver")
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+}
