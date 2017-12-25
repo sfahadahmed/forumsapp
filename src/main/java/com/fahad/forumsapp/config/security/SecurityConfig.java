2f300689410437307f7332df5a8a80f8b66f7084
sfahadahmed <sfahadahmed@gmail.com>
2017-12-25 20:16:48 +0500

    Disable CSRF tokens for Spring Security.

--- src/main/java/com/fahad/forumsapp/config/security/SecurityConfig.java
+++ src/main/java/com/fahad/forumsapp/config/security/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .httpBasic()
             .authenticationEntryPoint(authenticationEntryPoint);
 
+        http.csrf().disable();
+
         http.addFilterAfter(new CustomFilter(),
             BasicAuthenticationFilter.class);
     }
