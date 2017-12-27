d6a489dc03a1dfb8e38ed4cefe355541f1d391b8
sfahadahmed <sfahadahmed@gmail.com>
2017-12-27 20:44:09 +0500

    Adding support for role repository

---
+++ src/main/java/com/fahad/forumsapp/repos/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.fahad.forumsapp.repos;
+
+import com.fahad.forumsapp.models.Role;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author Fahad Ahmed
+ */
+public interface RoleRepository extends CrudRepository<Role, Long> {
+}
