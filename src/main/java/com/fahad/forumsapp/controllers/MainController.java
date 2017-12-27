a177847a96b606529995fa0d19caede342a72677
sfahadahmed <sfahadahmed@gmail.com>
2017-12-27 23:36:32 +0500

    Adding controller mapping handler for show topic feature.

--- src/main/java/com/fahad/forumsapp/controllers/MainController.java
+++ src/main/java/com/fahad/forumsapp/controllers/MainController.java
@@ -1,6 +1,10 @@
 package com.fahad.forumsapp.controllers;
 
+import com.fahad.forumsapp.models.Topic;
 import com.fahad.forumsapp.repos.CategoryRepository;
+import com.fahad.forumsapp.repos.PostRepository;
+import com.fahad.forumsapp.repos.TopicRepository;
+import com.fahad.forumsapp.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,9 +22,31 @@ public class MainController {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @GetMapping({"", "/forums"})
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("")
     public String home(Model model){
         model.addAttribute("categories", categoryRepository.findAll());
         return "forums/main";
+    }
+
+    @GetMapping("/showtopic")
+    public String viewTopic(@RequestParam long id, Model model){
+        Topic topic = topicRepository.findOne(id);
+
+        if(topic != null) {
+            model.addAttribute("topic", topic);
+            return "forums/showtopic";
+        }
+        else {
+            return "forums/main";
+        }
     }
 }
