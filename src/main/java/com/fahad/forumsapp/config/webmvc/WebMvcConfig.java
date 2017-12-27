0c97c0935b1aab61ed1ecf0314c085ec16aacca5
sfahadahmed <sfahadahmed@gmail.com>
2017-12-27 20:41:41 +0500

    Adding initial Spring MVC Java configuration

---
+++ src/main/java/com/fahad/forumsapp/config/webmvc/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.fahad.forumsapp.config.webmvc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @author Fahad Ahmed
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("/webjars/**")
+            .addResourceLocations("/webjars/");
+    }
+}
