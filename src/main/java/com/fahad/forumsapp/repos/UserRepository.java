1b75fa0260509224336e03480d274baba8c975a6
sfahadahmed <sfahadahmed@gmail.com>
2018-01-05 13:09:09 +0500

    Adding new lookup methods to the User repository

--- src/main/java/com/fahad/forumsapp/repos/UserRepository.java
+++ src/main/java/com/fahad/forumsapp/repos/UserRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.repository.CrudRepository;
  * @author Fahad Ahmed
  */
 public interface UserRepository extends CrudRepository<User, Long> {
+    public User findByLastName(String lastName);
+    public User findByUsername(String username);
+    public User findByEmail(String email);
 }
